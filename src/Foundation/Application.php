1bc640f061f6f5aa86ea1d5cd431a533d1fd8b35
shuchao <shuchao@medlinker.net>
2015-07-21 21:18:20 +0800

    ...

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -17,12 +17,15 @@ class Application
 	protected $rootPath;
 
 	/**
+	 * The container for the path.
+	 */
+	protected $container;
+
+	/**
 	 * Create a new Canon application instance.
 	 */
 	public function __construct($rootPath = null)
 	{
-
-
 		$rootPath && $this->setRootPath($rootPath);
 	}
 
@@ -42,6 +45,35 @@ class Application
 	public function setRootPath($rootPath)
 	{
 		$this->rootPath = rtrim($rootPath, '\/');
+		$this->bingPathToContainer();
+	}
+
+	/**
+	 * Bind the path to container.
+	 */
+	public function bingPathToContainer()
+	{
+		foreach (['root', 'config'] as $path) {
+			$this->container[$path] = $this->{$path.'Path'}();
+		}
+	}
+
+	/**
+	 * Get the rootPath for the application.
+	 * @return string
+	 */
+	public function rootPath()
+	{
+		return $this->rootPath;
+	}
+
+	/**
+	 * Get the configPath for the application.
+	 * @return string
+	 */
+	public function configPath()
+	{
+		return $this->rootPath.DIRECTORY_SEPARATOR.'config';
 	}
 
 	/**
---
+++ src/Foundation/ConfigLoader.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Canon\Foundation;
+
+class ConfigLoader
+{
+	
+}
